e9d479747510fd19f36733f01153a902182aa6ed
rakumarr <rajesh.msse@gmail.com>
2019-05-02 08:11:14 +0530

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HomeComponent } from './home/home.component';
 import { SocialLoginModule, AuthServiceConfig, GoogleLoginProvider } from 'angularx-social-login';
 import { LoginComponent } from './login/login.component';
 import { HttpClientModule } from '@angular/common/http';
+// import { AuthService } from './auth/auth.service';
 
 
 // const config = new AuthServiceConfig ([{
