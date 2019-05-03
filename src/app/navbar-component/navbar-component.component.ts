790b0d3cb20e04c4dd664bedc8ae0e0b172e78d8
rakumarr <rajesh.msse@gmail.com>
2019-05-04 01:35:49 +0530

    2 files modified

--- src/app/navbar-component/navbar-component.component.ts
+++ src/app/navbar-component/navbar-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticateService } from './../auth/auth.service';
 
 @Component({
   selector: 'app-navbar-component',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthenticateService) { }
 
   ngOnInit() {
+  }
+
+  logout(){
+    this.authService.signOut;
   }
 
 }
